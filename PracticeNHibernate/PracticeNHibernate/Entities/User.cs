f4a167f86825d384d2994f0d999c8c583857257c
Kayan Silva <kayanf.silva@outlook.com>
2019-03-08 15:10:26 -0300

    Adição de mais conteudo de aprendizado

---
+++ PracticeNHibernate/PracticeNHibernate/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace PracticeNHibernate.Entities
+{
+    public class User
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
---
+++ PracticeNHibernate/PracticeNHibernate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PracticeNHibernate.Infra;
+
+namespace PracticeNHibernate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NHibernateHelper.GeraSchema();
+
+            Console.Read();
+        }
+    }
+}
