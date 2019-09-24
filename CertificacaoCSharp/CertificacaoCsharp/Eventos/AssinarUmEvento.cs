43aaf24b73cda532c1bbbae65a4ae4d37ab45d50
Kayan Florencio da Silva <kayan.silva@iterative.com.br>
2019-09-24 17:26:18 -0300

    Adição de mais codigo

---
+++ CertificacaoCSharp/CertificacaoCsharp/Eventos/AssinarUmEvento.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CertificacaoCsharp.Eventos
+{
+    class AssinarUmEvento
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Campainha2 campainha = new Campainha2();
+                campainha.OnCampainhaTocou2 += CampainhaTocou3;
+                campainha.OnCampainhaTocou2 += CampainhaTocou2;
+                Console.WriteLine("A campainha será tocada.");
+                campainha.Tocar("101");
+
+                campainha.OnCampainhaTocou2 -= CampainhaTocou3;
+                Console.WriteLine("A campainha será tocada.");
+                campainha.Tocar("202");
+            }
+            catch (AggregateException e)
+            {
+                foreach (var exc in e.InnerExceptions)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void CampainhaTocou3(object sender, CampainhaEventArgs args)
+        {
+            Console.WriteLine("A campainha tocou no apartamento " + args.Apartamento + " .(1)");
+            throw new Exception("Ocorreu um erro em CampainhaTocou1");
+        }
+        static void CampainhaTocou2(object sender, CampainhaEventArgs args)
+        {
+            Console.WriteLine("A campainha tocou no apartamento " + args.Apartamento + " .(2)");
+            throw new Exception("Ocorreu um erro em CampainhaTocou2");
+        }
+    }
+
+    class Campainha2
+    {
+        public event EventHandler<CampainhaEventArgs> OnCampainhaTocou2;
+
+        public void Tocar(string apartamento)
+        {
+            List<Exception> erros = new List<Exception>();
+            foreach (var manipulador in OnCampainhaTocou2.GetInvocationList())
+            {
+                try
+                {
+                    manipulador.DynamicInvoke(this, new CampainhaEventArgs(apartamento));
+                }
+                catch (Exception e)
+                {
+                    erros.Add(e.InnerException);
+                }
+            }
+
+            throw new AggregateException(erros);
+        }
+    }
+
+    class CampainhaEventArgs : EventArgs
+    {
+        public CampainhaEventArgs(string apartamento) => Apartamento = apartamento;
+
+        public string Apartamento { get; }
+    }
+}
---
+++ CertificacaoCSharp/CertificacaoCsharp/Eventos/CriarUmEvento.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CertificacaoCsharp.Eventos
+{
+    class CriarUmEvento
+    {
+        static void Main(string[] args)
+        {
+            Campainha1 campainha = new Campainha1();
+            campainha.OnCampainhaTocou1 += CampainhaTocou1;
+            campainha.OnCampainhaTocou1 += CampainhaTocou2;
+            Console.WriteLine("A campainha será tocada.");
+            campainha.Tocar();
+
+            campainha.OnCampainhaTocou1 -= CampainhaTocou1;
+            Console.WriteLine("A campainha será tocada.");
+            campainha.Tocar();
+
+            Console.ReadKey();
+
+        }
+
+        static void CampainhaTocou1()
+        {
+            Console.WriteLine("A campainha tocou.(1)");
+        }
+        static void CampainhaTocou2()
+        {
+            Console.WriteLine("A campainha tocou.(2)");
+        }
+    }
+
+    class Campainha1
+    {
+        public Action OnCampainhaTocou1 { get; set; }
+
+        public void Tocar()
+        {
+            OnCampainhaTocou1?.Invoke();
+        }
+    }
+}
