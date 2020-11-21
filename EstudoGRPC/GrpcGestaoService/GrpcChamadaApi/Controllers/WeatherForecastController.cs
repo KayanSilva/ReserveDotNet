818a56d77f56449d0b7d9053d370bb3e7229a9ef
Kayan Florencio <kayanf.silva@outlook.com>
2020-11-21 19:21:51 -0300

    Realizando estudos com GRPC em ambiente de container com .Net 5.0

---
+++ EstudoGRPC/GrpcGestaoService/GrpcChamadaApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using Grpc.Net.Client;
+using GrpcGestaoClient.Protos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GrpcChamadaApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{mensagem}/{testado}/{status}")]
+        public async Task<IActionResult> Get(string mensagem, bool testado, int status)
+        {
+            var httpHandler = new HttpClientHandler
+            {
+                // Return `true` to allow certificates that are untrusted/invalid
+                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            };
+
+            using var channel = GrpcChannel.ForAddress("https://host.docker.internal:32788", new GrpcChannelOptions { HttpHandler = httpHandler });
+            var client = new Gestao.GestaoClient(channel);
+            var reply = await client.ChamadaTesteServiceAsync(new ExampleRequest { Mensagem = mensagem, Testado = testado, Status = status });
+
+            return new ObjectResult(reply) { StatusCode = StatusCodes.Status200OK };
+        }
+    }
+}
---
+++ EstudoGRPC/GrpcGestaoService/GrpcGestaoService/Services/GestaoService.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Core;
+using GrpcGestaoService.Protos;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GrpcGestaoService.Services
+{
+    public class GestaoService : Gestao.GestaoBase
+    {
+        private readonly ILogger<GestaoService> _logger;
+
+        public GestaoService(ILogger<GestaoService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ExampleResponse> ChamadaTesteService(ExampleRequest exampleRequest, ServerCallContext serverCallContext)
+        {
+            var response = new ExampleResponse { Id = exampleRequest.Status, Nome = $"{exampleRequest.Mensagem} Testado: {exampleRequest.Testado}" };
+            return Task.FromResult(response);
+        }
+    }
+}
