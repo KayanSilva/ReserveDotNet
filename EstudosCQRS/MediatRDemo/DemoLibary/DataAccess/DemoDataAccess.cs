4bfb48a81bc66107b2172f4c9859bae1b77763d3
Kayan Florencio <kayanf.silva@outlook.com>
2021-04-05 22:17:26 -0300

    Start no treinamento de CQRS.

---
+++ EstudosCQRS/MediatRDemo/DemoLibary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using DemoLibary.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DemoLibary.DataAccess
+{
+    public class DemoDataAccess : IDataAccess
+    {
+        private List<PersonModel> people = new();
+
+        public DemoDataAccess()
+        {
+            people.Add(new PersonModel { Id = 1, FirstName = "Kayan", LastName = "Silva" });
+            people.Add(new PersonModel { Id = 2, FirstName = "Kalahan", LastName = "Florencio" });
+        }
+
+        public List<PersonModel> GetPeople()
+        {
+            return people;
+        }
+
+        public PersonModel InsertPeople(string firstName, string lastName)
+        {
+            PersonModel p = new() { FirstName = firstName, LastName = lastName };
+            p.Id = people.Max(x => x.Id) + 1;
+            people.Add(p);
+            return p;
+        }
+    }
+}
---
+++ EstudosCQRS/MediatRDemo/DemoLibary/DataAccess/IDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using DemoLibary.Models;
+using System.Collections.Generic;
+
+namespace DemoLibary.DataAccess
+{
+    public interface IDataAccess
+    {
+        List<PersonModel> GetPeople();
+
+        PersonModel InsertPeople(string firstName, string lastName);
+    }
+}
